fc55f2e9c11e7cfe64dfea3655f93c7b5d81cc69
Marc-André Lureau <marcandre.lureau@gmail.com>
2013-09-13 16:14:10 +0200

    worker: minor simplifcation

--- server/red_worker.c
+++ server/red_worker.c
@@ -1461,16 +1461,18 @@ static inline Shadow *__find_shadow(TreeItem *item)
     return ((DrawItem *)item)->shadow;
 }
 
-static inline Ring *ring_of(RedWorker *worker, Ring *ring, TreeItem *item)
+/* FIXME: weird function: return container ring, or ring... */
+static inline Ring *ring_of(Ring *ring, TreeItem *item)
 {
     return (item->container) ? &item->container->items : ring;
 }
 
-static inline int __contained_by(RedWorker *worker, TreeItem *item, Ring *ring)
+/* FIXME: weird function: true if item has some parent container == ring, or has no immediate container*/
+static inline int __contained_by(TreeItem *item, Ring *ring)
 {
     spice_assert(item && ring);
     do {
-        Ring *now = ring_of(worker, ring, item);
+        Ring *now = ring_of(ring, item);
         if (now == ring) {
             return TRUE;
         }
@@ -1512,8 +1514,8 @@ static inline void __exclude_region(RedWorker *worker, Ring *ring, TreeItem *ite
                     region_exclude(&shadow->on_hold, &and_rgn);
                     region_or(rgn, &and_rgn);
                     // in flat representation of current, shadow is always his owner next
-                    if (!__contained_by(worker, (TreeItem*)shadow, *top_ring)) {
-                        *top_ring = ring_of(worker, ring, (TreeItem*)shadow);
+                    if (!__contained_by((TreeItem*)shadow, *top_ring)) {
+                        *top_ring = ring_of(ring, (TreeItem*)shadow);
                     }
                 }
             } else {
@@ -1532,8 +1534,8 @@ static inline void __exclude_region(RedWorker *worker, Ring *ring, TreeItem *ite
                 region_exclude(rgn, &and_rgn);
                 if ((shadow = __find_shadow(item))) {
                     region_or(rgn, &shadow->on_hold);
-                    if (!__contained_by(worker, (TreeItem*)shadow, *top_ring)) {
-                        *top_ring = ring_of(worker, ring, (TreeItem*)shadow);
+                    if (!__contained_by((TreeItem*)shadow, *top_ring)) {
+                        *top_ring = ring_of(ring, (TreeItem*)shadow);
                     }
                 }
             }
